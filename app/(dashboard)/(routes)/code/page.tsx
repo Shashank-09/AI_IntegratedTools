e34820f709394b1606efe6977d49566092385aed
Shashank-09 <shashanktulsikar979@gmail.com>
2023-08-06 00:03:23 +0530

    seventh commit

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -124,7 +124,19 @@ const onSubmit = async (values: z.infer<typeof formSchema>) => {
                     )}
                     >
                         {messages.role === "user" ? <UserAvatar /> : <BotAvatar />}
-                       <ReactMarkdown>
+                       <ReactMarkdown
+                         components={{
+                           pre: ({ node, ...props}) => (
+                            <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                               <pre {...props} />
+                            </div>
+                           ),
+                           code: ({ node ,  ...props }) => (
+                            <code className="bg-black/10 rounded-lg p-1" {...props} />
+                           )   
+                         }}
+                         className="text-sm overflow-hidden leading-7"
+                       >
                         {messages.content || ""}
                        </ReactMarkdown>
                     </div>
